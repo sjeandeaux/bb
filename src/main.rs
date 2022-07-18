52d61b651dc40e094881387d16adc54dae693d45
Stephane Jeandeaux <stephane.jeandeaux@gmail.com>
2022-07-18 15:12:58 +0200

    [refactoring] doc

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,14 @@ fn main() {
   let cmd = clap::Command::new("bb")
         .bin_name("bb")
         .subcommand_required(true)
-        .subcommand(clap::Command::new("pr").subcommand_required(true).subcommand(clap::SubCommand::with_name("create")));
-    let matches = cmd.get_matches();
-    let matches = match matches.subcommand() {
-        Some(("pr", matches)) => matches,
+        .subcommand(pr_command());
+    let principale = cmd.get_matches();
+    match principale.subcommand() {
+        Some(("pr", pr_command)) => println!("pr: {:?}", pr_command.subcommand()),
         _ => unreachable!("clap should ensure we don't get here"),
     };
-    println!("{:?}", matches);
+}
+
+fn pr_command() -> clap::Command<'static> {
+ clap::Command::new("pr").about("Manage pull requests").subcommand_required(true).subcommand(clap::SubCommand::with_name("create").about("Create a pull request").arg(clap::Arg::with_name("fill")))
 }
